3527ccb710fd02cb0ffb4364507f8c6f9e29f4ea
Samar Manjeshwar <samarmanjeshwar@Samars-MacBook-Air.local>
2025-05-11 20:36:07 -0400

    Card styling

--- pages/api/google/googleSheets.tsx
+++ pages/api/google/googleSheets.tsx
@@ -7,7 +7,6 @@ export const getSheetData = async () => {
       type: "service_account",
       project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
       private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
-      //   private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
       private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, "\n"), //https://stackoverflow.com/questions/72332485/err-ossl-unsupported-when-trying-to-use-google-spreadsheet-library-with-typscrip
       client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
       universe_domain: "googleapis.com",
@@ -20,7 +19,7 @@ export const getSheetData = async () => {
     range: "responses!A:AL",
   });
   const sheetInfo = data.data.values.filter((row) => {
-    return row[0] !== ''
-  })
+    return row[0] !== "";
+  });
   return sheetInfo.slice(1);
 };
